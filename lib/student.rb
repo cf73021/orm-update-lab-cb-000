dbde8d9eaceacc905d1ccb1f5b9aae73d524919c
Chris Franklin <cf73021@gmail.com>
2018-10-08 20:21:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,10 @@ class Student
 attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+  end
 
   def self.create_table
     sql = <<-SQL
